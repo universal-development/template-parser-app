4db8cc83e8a80390ec7870ec5099819e675ad771
Denis O <denis@universal-development.com>
2021-10-12 22:50:02 +0300

    basic parsing app

--- app/src/main/java/com/unidev/parserapp/App.java
+++ app/src/main/java/com/unidev/parserapp/App.java
@@ -1,7 +1,16 @@
 package com.unidev.parserapp;
 
+import com.unidev.httpclient.okhttp.OkHttp;
+import com.unidev.parser.Parser;
+import org.apache.commons.lang3.StringUtils;
+
 public class App {
 
     public static void main(String[] args) {
+        String page = new OkHttp().get("http://canyouseeme.org/");
+        Parser parser = new Parser();
+        page = parser.clean(page);
+        String ip = StringUtils.substringBetween(page, "id=\"ip\" type=\"text\" value=\"", "\"");
+        System.out.println(ip);
     }
 }
